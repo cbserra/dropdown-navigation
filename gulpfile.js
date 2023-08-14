49f2db84ea1ec790a56858980d224d40e30e0477
Chris Serra <789723+cbserra@users.noreply.github.com>
2023-08-14 00:27:02 -0700

    refactor: 🐛 cleanup; fix menu transitions

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { series, parallel, watch, src, dest } = require('gulp');
 const uglify = require('gulp-uglify');
 const del = require('del');
+// import {deleteSync} from 'del';
 const sass = require('gulp-sass')(require('sass'));
 const pipeline = require('readable-stream').pipeline;
 const cleanCSS = require('gulp-clean-css');
@@ -9,34 +10,60 @@ function clean() {
     return del(['public/*.js', 'public/*.css']);
 }
 
+function cleanJs() {
+    return del('public/*.js');
+}
+
 function cssTranspile() {
-    return src('./src/*.scss')
+    return src('src/*.scss')
         .pipe(
             sass({
                 includePaths: ['node_modules/normalize-scss/sass'],
             }).on('error', sass.logError),
         )
-        .pipe(dest('./public'));
+        .pipe(dest('public'));
 }
 
 function cssMinify() {
-    return src('build/*.css')
+    return src('public/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(dest('build'));
+        .pipe(dest('public'));
 }
 
 function jsMinify() {
-    return pipeline(src('./src/*.js'), uglify(), dest('public'));
+    return pipeline(src('src/*.js'), uglify(), dest('public'));
+}
+
+function jsCopy() {
+    return pipeline(src('src/*.js'), dest('public'));
 }
 
 exports.clean = clean;
 
 exports.build = series(clean, cssTranspile, parallel(cssMinify, jsMinify));
 
-exports.watch = function () {
-    exports.build;
-    watch('src/styles.scss', cssTranspile, cssMinify);
-    watch('src/scripts.js', jsMinify);
-};
+function watchChanges() {
+    // You can use a single task
+    watch('src/*.scss', cssTranspile);
+    // Or a composed task
+    watch('src/*.js', series(cleanJs, jsCopy));
+}
 
-exports.default = exports.build;
+exports.watch = watchChanges;
+
+exports.default = watchChanges;
+
+// exports.watch = function () {
+//     exports.build;
+//     watch('src/styles.scss', cssTranspile, cssMinify);
+//     watch('src/scripts.js', jsMinify);
+// };
+
+// exports.default = exports.build;
+
+// exports.default = function () {
+//     // You can use a single task
+//     watch('src/*.scss', cssTranspile, cssMinify);
+//     // Or a composed task
+//     watch('src/*.js', series(cleanJs, jsMinify));
+// };
--- src/scripts.js
+++ src/scripts.js
@@ -12,20 +12,128 @@ const companyLink = document.querySelector('.company-link');
 const companyItem = document.querySelector('.company-item');
 companyLink.addEventListener('click', (evt) => toggleSubMenus(evt, companyItem));
 
-const closeLink = document.querySelector('.close-menus');
-const openLink = document.querySelector('.open-menus');
+// const closeLink = document.querySelector('.close-menus');
+// const openLink = document.querySelector('.open-menus');
 const overlayContainer = document.querySelector('.overlay-container');
-const menus = document.querySelector('.menus');
+const menus = overlayContainer.querySelector('.menus');
+
+const toggleMenuLabel = document.querySelector('.toggle-sidebar-menu-lbl');
+// const openMenuLabel = document.querySelector('.open-sidebar-menu-lbl');
+// const closeMenuLabel = document.querySelector('.close-sidebar-menu-lbl');
+
+// const toggleMenuOpen = document.querySelector('.toggle-sidebar-menu input[type="checkbox"]:checked');
+// const toggleMenuClose = document.querySelector('.toggle-sidebar-menu input[type="checkbox"]');
 
 function toggleMobileMenus(_evt) {
+    console.log(`🚀 ~ toggleMobileMenus ~ _evt:`, _evt);
+    //     if (toggleMenuCheckbox.checked) {
+    //         menus.classList.add('slide-in');
+    //         setTimeout(() => overlayContainer.classList.toggle('show'), 500);
+    //     } else {
+    //         overlayContainer.classList.toggle('show');
+    //         setTimeout(() => menus.classList.toggle('slide-in'), 500);
+    //     }
+    // }
+
     if (menus.classList.contains('slide-in')) {
-        menus.classList.toggle('slide-in');
-        setTimeout(() => overlayContainer.classList.toggle('show'), 500);
+        menus.classList.remove('slide-in');
+        menus.classList.add('slide-out');
+        // setTimeout(() => overlayContainer.classList.add('show'), 500);
     } else {
-        overlayContainer.classList.toggle('show');
-        setTimeout(() => menus.classList.toggle('slide-in'), 500);
+        menus.classList.remove('slide-out');
+        menus.classList.add('slide-in');
+
+        // setTimeout(() => menus.classList.add('slide-in'), 500);
+    }
+
+    if (overlayContainer.classList.contains('show')) {
+        overlayContainer.classList.remove('show');
+        overlayContainer.classList.add('hide');
+
+        // setTimeout(() => menus.classList.remove('slide-out'), 500);
+    } else {
+        // setTimeout(() => overlayContainer.classList.add('show'), 500);
+        overlayContainer.classList.remove('hide');
+        overlayContainer.classList.add('show');
     }
 }
 
-closeLink.addEventListener('click', toggleMobileMenus);
-openLink.addEventListener('click', toggleMobileMenus);
+toggleMenuLabel.addEventListener('click', toggleMobileMenus);
+
+// function openMobileMenus(_evt) {
+//     console.log(`🚀 ~ openMobileMenus ~ _evt:`, _evt);
+//     if (toggleMenuCheckbox.checked) {
+//         menus.classList.add('slide-in');
+//         setTimeout(() => overlayContainer.classList.toggle('show'), 500);
+//     } else {
+//         overlayContainer.classList.toggle('show');
+//         setTimeout(() => menus.classList.toggle('slide-in'), 500);
+//     }
+// }
+
+// menus.classList.add('slide-in');
+// setTimeout(() => overlayContainer.classList.add('show'), 500);
+
+// if (menus.classList.contains('slide-in')) {
+//     menus.classList.remove('slide-in');
+//     menus.classList.add('slide-out');
+//     // setTimeout(() => overlayContainer.classList.add('show'), 500);
+// } else {
+//     menus.classList.remove('slide-out');
+//     menus.classList.add('slide-in');
+
+//     // setTimeout(() => menus.classList.add('slide-in'), 500);
+// }
+
+// if (overlayContainer.classList.contains('show')) {
+//     overlayContainer.classList.remove('show');
+//     overlayContainer.classList.add('hide');
+
+//     // setTimeout(() => menus.classList.remove('slide-out'), 500);
+// } else {
+//     // setTimeout(() => overlayContainer.classList.add('show'), 500);
+//     overlayContainer.classList.remove('hide');
+//     overlayContainer.classList.add('show');
+// }
+// }
+
+// function closeMobileMenus(_evt) {
+//     console.log(`🚀 ~ closeMobileMenus ~ _evt:`, _evt);
+//     if (toggleMenuCheckbox.checked) {
+//         menus.classList.add('slide-in');
+//         setTimeout(() => overlayContainer.classList.toggle('show'), 500);
+//     } else {
+//         overlayContainer.classList.toggle('show');
+//         setTimeout(() => menus.classList.toggle('slide-in'), 500);
+//     }
+// }
+
+// menus.classList.remove('slide-in');
+// setTimeout(() => overlayContainer.classList.remove('show'), 500);
+
+// if (menus.classList.contains('slide-in')) {
+//     menus.classList.add('slide-out');
+// } else {
+//     menus.classList.remove('slide-out');
+//     menus.classList.add('slide-in');
+
+//     // setTimeout(() => menus.classList.add('slide-in'), 500);
+// }
+
+// if (overlayContainer.classList.contains('show')) {
+//     overlayContainer.classList.remove('show');
+//     overlayContainer.classList.add('hide');
+
+//     // setTimeout(() => menus.classList.remove('slide-out'), 500);
+// } else {
+//     // setTimeout(() => overlayContainer.classList.add('show'), 500);
+//     overlayContainer.classList.remove('hide');
+//     overlayContainer.classList.add('show');
+// }
+// }
+
+// openMenuLabel.addEventListener('click', openMobileMenus);
+// closeMenuLabel.addEventListener('click', closeMobileMenus);
+
+// closeLink.addEventListener('click', toggleMobileMenus);
+// openLink.addEventListener('click', toggleMobileMenus);
